6207236af8d153b4f4c0ec70930fbbe6eb528aef
chanhnguyen <chanhnguyen@ChanhNguyenPC>
2017-01-31 12:33:08 +0200

    Coffee Maker UI add test javascript.js

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -4,45 +4,19 @@
  * and open the template in the editor.
  */
 
+
+//Not in use
 var req;
 var isIE;
 
-var infoList;
-var addButton;
-var makeCoffee;
+var onOffDisplay;
+var waterDisplay;
+var beanDisplay;
 
-function Ajax() {
-    $.ajax({
-        method: "POST",
-        url: "some.php",
-        data: {name: "John", location: "Boston"}
-    })
-        .done(function (msg) {
-            alert("Data Saved: " + msg);
-        });
-}
-
-function init() {
-    infoList = document.getElementById("info-list");
-    addButton = document.getElementById("add-button");
-    makeCoffee = document.getElementById("make-coffee");
-
-    addWater = document.getElementById("add-water");
-    addBean = document.getElementById("add-bean");
-    onOff = document.getElementById("onOff");
-    brew = document.getElementById("brew");
-
-    
-
-    //completeTable.style.top = getElementY(autoRow) + "px";
-}
-
-function doCompletion() {
-    var url = "autocomplete?action=complete&id=" + escape(infoList.value);
-    req = initRequest();
-    req.open("GET", url, true);
-    req.onreadystatechange = callback;
-    req.send(null);
+function init(){
+    onOffDisplay = document.getElementById("on-off-display");
+    waterDisplay = document.getElementById("water");
+    beanDisplay = document.getElementById("bean");
 }
 
 function initRequest() {
@@ -54,25 +28,5 @@ function initRequest() {
     } else if (window.ActiveXObject) {
         isIE = true;
         return new ActiveXObject("Microsoft.XMLHTTP");
-    }
-}
-
-function clearTable() {
-    if (infoList.getElementsByTagName("tr").length > 0) {
-        infoList.style.display = 'none';
-        for (loop = infoList.childNodes.length - 1; loop >= 0; loop--) {
-            infoList.removeChild(infoList.childNodes[loop]);
-        }
-    }
-}
-
-function callback() {
-
-    clearTable();
-
-    if (req.readyState === 4) {
-        if (req.status === 200) {
-            parseMessages(req.responseXML);
-        }
     }
 }
