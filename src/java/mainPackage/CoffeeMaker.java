82ae50081b5336595fa9fce226a87e372b8be185
chanhnguyen <chanhnguyen@ChanhNguyenPC>
2017-02-12 11:12:34 +0200

    add REST

--- src/java/mainPackage/CoffeeMaker.java
+++ src/java/mainPackage/CoffeeMaker.java
@@ -4,6 +4,14 @@ package mainPackage;
 import CoffeeMakerElectricComponent.*;
 import Container.*;
 
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +23,22 @@ import Container.*;
  *
  * @author chanhnguyen
  */
-public class CoffeeMaker {
+@Entity
+@XmlRootElement
+public class CoffeeMaker implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
     private final Container waterContainer;
     private final Container beanContainer;
